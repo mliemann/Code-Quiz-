81b14d7e2fe96c24b4f4c70e1939bde28290d10d
Marlon Liemann <mliemann@live.com>
2021-05-19 19:15:40 -0400

    fixed code for time & ensured the first page was working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,21 @@
 var highscores = document.querySelector(".scores")
 var timer = document.querySelector(".time")
-var content = document.querySelector(".questions")
+var alerts = document.querySelector(".questions")
 var secondsLeft = 100;
-
+var test = document.querySelector(".startbutton")
 var test1 = document.querySelector("#startbutton1")
 
-test1.addEventListener("click", setTime())
+test1.onclick = function setTime(){
 
-function setTime(){
 var timerInterval = setInterval(function() {
+    test.remove()
+    alerts.textContent= "You've started the test. "
     timer.textContent = secondsLeft + " seconds left";
     secondsLeft--;
     if(secondsLeft === 0) {
       clearInterval(timerInterval);
+      timer.textContent = 0
+      
     }
   }, 1000);
 }
