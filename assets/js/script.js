9873c950d68889cd33a06acf75808464d9817913
Marlon Liemann <mliemann@live.com>
2021-05-22 10:00:51 -0400

    added array

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,10 +8,12 @@ var test1 = document.querySelector("#startbutton1")
 var QI = document.querySelector("#questionInput")
 var remove1= document.querySelector("#questionRemove")
 var LQ=document.querySelector("#listQuestions")
-
+var namearray = []
+var scorearray =[]
 var score = 0
 var secondsLeft = 30;
-
+console.log(namearray)
+//renderhighscores
 highscores.onclick = function scores(){
   all.innerHTML=null;
       all1 = document.createElement('div');
@@ -22,7 +24,7 @@ highscores.onclick = function scores(){
       all3.setAttribute("id", "score");
       all3.classList.add("finalscores1")
       all.appendChild(all1);
-      all1.appendChild(all2);
+      all1.appendChild(all2)
       all1.appendChild(all3);
       var all4a = localStorage.getItem("all4a");
       var savescore1 = localStorage.getItem("savescore1");
@@ -33,25 +35,23 @@ highscores.onclick = function scores(){
 
       all2.textContent = all4a;
       all3.textContent = savescore1;
+
 }
 
 test1.onclick= function start(){
   test1.remove()
-  displayTime()
   startTimer()
    Q1();
    all1 = document.createElement('div');
    all2 = document.createElement('h1');
-   all2.innerHTML = "You Lost. Your Score is: 0" 
    all2.classList.add("#questionInput")
    all.appendChild(all1);
-   all1.appendChild(all2)
 }
 
 function deductTimeBy(seconds) {
   secondsLeft -= seconds;
-  startTimer()
   displayTime();
+  return
 }
 
 function displayTime() {
@@ -81,6 +81,7 @@ function startTimer() {
     all1.appendChild(all2)
   }
 
+  //question4
 function Q1() {
   QI.textContent= "1. What is a string?"
   remove1.remove()
@@ -89,7 +90,6 @@ function Q1() {
     "C. Items mapped directly to ranges.",
     "D. None of the above. "]
   var number = ['0','1','2','3']
- 
   for (i = 0; i < questions1.length; i++) {
      listItem = document.createElement('button');
      listItem.innerHTML = questions1[i];
@@ -135,6 +135,8 @@ function Q1() {
     deductTimeBy(10)
     Q2()
   }
+
+  //question3
   function Q2(){
   QI.textContent= "2. What is HTML?"
   var questions2 = [ "A. A  language for specifying how documents are presented to users.",
@@ -189,6 +191,8 @@ function Q1() {
     }
     
   }
+
+  //question2
   function Q3(){
     QI.textContent= "3. What is Javascript? "
     var questions2 = ["A. A  language for specifying how documents are presented to users.",
@@ -243,6 +247,8 @@ function Q1() {
       }
       
     }
+
+    //question 1
     function Q4(){
       QI.textContent= "4. What is CSS? "
       var questions2 = [ "A. A  language for specifying how documents are presented to users.",
@@ -295,58 +301,94 @@ function Q1() {
           getScore()
         
       }
-    }
-    function getScore(){
-      var finalscore = score * 25
-      console.log(finalscore)
-      var finalseconds = secondsLeft 
-      clearTimeout(totalTimeInterval);
-      timer.textContent =  "Clock stopped at: " + finalseconds ;
-      all.innerHTML = null;
-      all1 = document.createElement('div');
-      all2 = document.createElement('h1');
-      all4= document.createElement('input')
-      all3 =document.createElement('button');
-      all2.innerHTML = "Your Score is: " +finalscore;
-      all2.classList.add("#questionInput");
-      all3.classList.add('startbutton2');
-      all4.classList.add("input")
-      all.appendChild(all1);
-      all.appendChild(all4)
-      all1.appendChild(all2);
-      all.appendChild(all3);
-      all3.innerHTML = "Save your score?";
-      all3.onclick= function savescore(event){
-      event.preventDefault();
-      var all4a = document.querySelector(".input").value
-      var savescore1= finalscore
-      localStorage.setItem("all4a", all4a)
-      localStorage.setItem("savescore1", JSON.stringify(savescore1));
-      console.log(all4a)
-      console.log(savescore1)
-      renderscore()
+
     
-      function renderscore() {all.innerHTML=null;
-      all1 = document.createElement('div');
+  
+  }
+    console.log(namearray)
+    console.log(scorearray)
+}
+
+
+function init() {
+  var storedNames = JSON.parse(localStorage.getItem("namearray"));
+  var storedScores = JSON.parse(localStorage.getItem("scorearray"))
+
+  if (storedScores !== null || storedNames !== null) {
+    namearray = storedNames;
+    scorearray= storedScores
+}
+}
+
+  //compute final score
+  function getScore(){
+    var finalscore = score * 25
+    console.log(finalscore)
+    var finalseconds = secondsLeft;
+    clearTimeout(totalTimeInterval);
+    timer.textContent =  "Clock stopped at: " + finalseconds ;
+    all.innerHTML = null;
+    all1 = document.createElement('div');
+    all2 = document.createElement('h1');
+    all4= document.createElement('input')
+    all3 =document.createElement('button');
+    all2.innerHTML = "Your Score is: " +finalscore;
+    all2.classList.add("#questionInput");
+    all3.classList.add('startbutton2');
+    all4.classList.add("input")
+    all.appendChild(all1);
+    all.appendChild(all4)
+    all1.appendChild(all2);
+    all.appendChild(all3);
+    all3.innerHTML = "Save your score?";
+ 
+  //savescore to local storage
+  all3.onclick= function savescore(event){
+    event.preventDefault();
+    var namearray = []
+    var scorearray =[]
+    var all4a = document.querySelector(".input").value
+    var savescore1= finalscore
+    if (all4a === "" || savescore1 === "") {
+      return;
+    }
+    
+    scorearray.push(savescore1);
+    namearray.push(all4a)
+    localStorage.setItem("all4a", JSON.stringify (namearray))
+    localStorage.setItem("savescore1", JSON.stringify(scorearray));
+    console.log(namearray)
+    console.log(scorearray)
+    renderscore()
+  
+  //renderscore
+  function renderscore() {all.innerHTML=null;
+    all1 = document.createElement('div');
+    all.appendChild(all1);
+    
+    for (var i = 0; i < namearray.length; i++) {
+      var namearray1 = namearray[i];
+      var scorearray1 = scorearray[i]
+    
+      console.log(namearray1)
+      console.log(scorearray1)
+    
       all2 = document.createElement('h1');
       all2.setAttribute("id", "name");
       all2.classList.add("finalscores1")
+      all2.textContent = namearray1;
+      all2.setAttribute("data-index", i);
+    
       all3 = document.createElement('h1');
       all3.setAttribute("id", "score");
       all3.classList.add("finalscores1")
-      all.appendChild(all1);
+      all3.textContent = scorearray1;
+      all3.setAttribute("data-index", i);
+    
       all1.appendChild(all2);
       all1.appendChild(all3);
-      var all4a = localStorage.getItem("all4a");
-      var savescore1 = localStorage.getItem("savescore1");
-
-      if (!all4a || !savescore1) {
-        return;
-      }
-
-      all2.textContent = all4a;
-      all3.textContent = savescore1;
-         }
       }
     }
-  }
+    }
+}
+
